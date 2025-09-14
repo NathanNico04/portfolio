bb61a73d06076cac385955ad0b11887afad58698
Nathan Nicolessi <nathan.nicolessi@epita.fr>
2025-09-14 21:27:30 +0200

    suppresion

--- portfolio/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { PortfolioComponent } from './portfolio/portfolio.component';
- 
-export const routes: Routes = [
-    {path:'',component: PortfolioComponent},
-];
